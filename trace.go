9073ea1279060fb3ee690ad2d0ee28a1139d24c8
David Stainton <dstainton415@gmail.com>
2014-12-23 07:27:19 +0000

    Code cleanup; remove hardcoded values
    we allow two configuration options:
    - set how many repeats of a given TTL we should send out
    - how many packets should traverse the flow for every packet
    that we mangle

--- trace.go
+++ trace.go
@@ -9,18 +9,34 @@ package main
 import (
 	"code.google.com/p/gopacket"
 	"code.google.com/p/gopacket/layers"
-	"fmt"
 	"github.com/david415/go-netfilter-queue"
 )
 
 type NFQueueTraceroute struct {
-	q       *netfilter.NFQueue
-	packets <-chan netfilter.NFPacket
-	count   int
-	ttl     uint8
-	done    chan bool
+	q            *netfilter.NFQueue
+	packets      <-chan netfilter.NFPacket
+	done         chan bool
+	ttl          uint8
+	ttlRepeat    int
+	ttlRepeatMax int
+	mangleFreq   int
+	count        int
 }
 
+// conduct an nfqueue tcp traceroute;
+// - send each TTL out ttlRepeatMax number of times.
+// - only mangle a packet's TTL after mangleFreq number
+// of packets have traversed the flow
+func NewNFQueueTraceroute(ttlRepeatMax, mangleFreq int) NFQueueTraceroute {
+	var nfqTrace = NFQueueTraceroute{}
+	nfqTrace.mangleFreq = mangleFreq
+	nfqTrace.ttlRepeatMax = ttlRepeatMax
+	nfqTrace.ttlRepeat = 1
+	nfqTrace.count = 1
+	return nfqTrace
+}
+
+// XXX perhaps make the infinite for loop execute in another goroutine?
 func (n *NFQueueTraceroute) Start() {
 	var err error
 	// XXX adjust these parameters
@@ -35,7 +51,6 @@ func (n *NFQueueTraceroute) Start() {
 		case p := <-n.packets:
 			n.processPacket(p)
 		case <-n.done:
-			//close(n.packets)
 			close(n.done)
 			break
 		}
@@ -47,29 +62,32 @@ func (n *NFQueueTraceroute) Stop() {
 	n.q.Close()
 }
 
-/* XXX todo: Make this "iterator" smarter;
-   perhaps increment after repeating a TTL 3 times or so.
-*/
-func (n *NFQueueTraceroute) currentTTL() uint8 {
-	n.ttl += 1
+// return the next TTL which shall be used to conduct a traceroute
+// each TTL is used n.ttlRepeatMax number of times.
+func (n *NFQueueTraceroute) nextTTL() uint8 {
+	n.ttlRepeat += 1
+	if n.ttlRepeat > n.ttlRepeatMax {
+		n.ttl += 1
+		n.ttlRepeat = 0
+	}
 	return n.ttl
 }
 
-/* XXX todo: we can fuck with some unknown ratio of packets
-for a given flow without brekaing it. Perhaps this can be dynamic based on
-packets per second in a given flow?
-*/
+// given a packet we decided weather or not to mangle the TTL
+// for our tracerouting purposes. we mangle a packet's TTL only
+// after we've seen mangleFreq number of packets traverse the flow.
 func (n *NFQueueTraceroute) processPacket(p netfilter.NFPacket) {
 	n.count += 1
-	if n.count%67 == 0 {
-		p.SetModifiedVerdict(netfilter.NF_ACCEPT, serializeWithTTL(p.Packet, n.currentTTL()))
+	if n.count%n.mangleFreq == 0 {
+		p.SetModifiedVerdict(netfilter.NF_ACCEPT, serializeWithTTL(p.Packet, n.nextTTL()))
 	} else {
 		p.SetVerdict(netfilter.NF_ACCEPT)
 	}
 }
 
 // XXX fixme: make me work with IPv6!
-// This function takes a
+// This function takes a gopacket.Packet and a TTL
+// and returns a byte array of the serialized packet with the specified TTL
 func serializeWithTTL(p gopacket.Packet, ttl uint8) []byte {
 	ipLayer := p.Layer(layers.LayerTypeIPv4)
 	if ipLayer == nil {
@@ -101,9 +119,6 @@ iptables -A OUTPUT -j NFQUEUE --queue-num 0 -p tcp --dport 2666
 
 ***/
 func main() {
-	n := NFQueueTraceroute{
-		count: 1,
-		ttl:   1,
-	}
+	n := NewNFQueueTraceroute(3, 66)
 	n.Start()
 }
